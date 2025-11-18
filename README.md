8e81cd63efbc626e89683b82bdf8e56f28963b30
romelllo <romannovikov526@gmail.com>
2025-11-18 20:29:22 +0000

    README, wezterm and zed settings updates

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ## dotfiles
-This repository contains my personal dotfiles and configuration files for various applications and tools.
+This repository contains dotfiles and configuration files for various applications and tools.
+
+### Contents
+- `wezterm`: Configuration files for WezTerm terminal emulator.
+- `zed`: Configuration files for Zed text editor.
+- `zsh` : Configuration files for Zsh shell.
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,11 +1,21 @@
 -- Pull in the wezterm API
 local wezterm = require('wezterm')
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- Front-End
+config.front_end = "WebGpu"  -- uses Metal on macOS
+
+config.treat_east_asian_ambiguous_width_as_wide = true
+config.keys = {
+  {key="k", mods="CMD|SHIFT", action=act.ScrollToPrompt(-1)},
+  {key="j", mods="CMD|SHIFT", action=act.ScrollToPrompt(1)},
+}
+
 -- Theme
-config.color_scheme = 'Everforest Dark (Gogh)'
+config.color_scheme = 'Mar (Gogh)'
 
 -- Return the configuration to wezterm
 return config
