e5515d0cac6ec3b2b0689cf2012d6a0f0a8a128d
romelllo <romannovikov526@gmail.com>
2026-01-18 20:51:44 +0000

    Added new features to wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,6 +8,13 @@ local config = wezterm.config_builder()
 -- Front-End
 config.front_end = "WebGpu"  -- uses Metal on macOS
 
+-- Font
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 12
+
+-- Window decorations
+config.window_decorations = "RESIZE"
+
 config.treat_east_asian_ambiguous_width_as_wide = true
 config.keys = {
   {key="k", mods="CMD|SHIFT", action=act.ScrollToPrompt(-1)},
@@ -16,6 +23,8 @@ config.keys = {
 
 -- Theme
 config.color_scheme = 'Everforest Dark (Gogh)'
+-- config.window_background_opacity = 0.9
+-- config.macos_window_background_blur = 10
 
 -- Return the configuration to wezterm
 return config
