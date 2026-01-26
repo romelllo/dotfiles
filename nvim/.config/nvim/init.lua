e5298228a0d2f19f222e34717f6feb49c9a412bd
rnovikov <roman.novikov@cruk.manchester.ac.uk>
2026-01-26 12:59:12 +0000

    Added everforest colorsheme to nvim init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -867,6 +867,7 @@ require("lazy").setup({
 			sources = {
 				default = { "lsp", "path", "snippets", "lazydev" },
 				providers = {
+					version = false,
 					lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
 				},
 			},
@@ -887,25 +888,22 @@ require("lazy").setup({
 		},
 	},
 
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
+	{ -- Everforest colorscheme
+		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000,
 		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("tokyonight").setup({
-				styles = {
-					comments = { italic = false }, -- Disable italics in comments
-				},
+			local everforest = require("everforest")
+
+			everforest.setup({
+				background = "medium", -- "soft" | "medium" | "hard"
+				italics = false,
+				disable_italic_comments = true,
 			})
 
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			everforest.load()
+			-- vim.cmd.colorscheme("everforest")
 		end,
 	},
 
@@ -957,7 +955,7 @@ require("lazy").setup({
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.config", -- Sets main module to use for opts
+		main = "nvim-treesitter.configs", -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
 			ensure_installed = {
